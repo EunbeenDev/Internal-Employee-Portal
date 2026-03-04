140f16036948918ce27c2f899bc899fec47d46d5
EunbeenDev <ros4312@naver.com>
2026-03-04 17:14:47 +0900

    Initial commit

---
+++ src/main/java/com/internalemployeeportal/InternalEmployeePortalApplication.java
@@ -0,0 +1,13 @@
+package com.internalemployeeportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternalEmployeePortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternalEmployeePortalApplication.class, args);
+    }
+
+}
