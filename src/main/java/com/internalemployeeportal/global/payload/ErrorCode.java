d2d9795ab3d980cd2867d3ef9e36400593d7c400
EunbeenDev <ros4312@naver.com>
2026-03-05 17:56:19 +0900

    feat: 예외 처리 커스텀 구조 구현
    BusinessException으로 하위 분리

---
+++ src/main/java/com/internalemployeeportal/global/payload/ErrorCode.java
@@ -0,0 +1,35 @@
+package com.internalemployeeportal.global.payload;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorCode {
+
+    // Server (SVR)
+    INTERNAL_SERVER_ERROR(500, "SVR001", "서버 내부 오류입니다."),
+
+    INVALID_PARAMETER(400, "E001", "잘못된 요청 데이터 입니다."),
+    INVALID_REPRESENTATION(400, "E002", "잘못된 표현 입니다."),
+    INVALID_FILE_PATH(400, "E003", "잘못된 파일 경로 입니다."),
+    INVALID_OPTIONAL_ISPRESENT(400, "E004", "해당 값이 존재하지 않습니다."),
+    INVALID_CHECK(400, "E005", "해당 값이 유효하지 않습니다."),
+    INVALID_AUTHENTICATION(400, "E006", "잘못된 인증입니다."),
+    INVALID_TOKEN(400, "E007", "잘못된 토큰입니다."),
+    NOT_FOUND(404, "E008", "해당 데이터를 찾을 수 없습니다."),
+    DUPLICATE_ERROR(409, "E009", "중복된 데이터가 존재합니다."),
+
+    // User (USR)
+    USER_DUPLICATE(409, "USR002", "이미 존재하는 사용자입니다."),
+    USER_NOT_FOUND(404, "USR001", "해당 사용자를 찾을 수 없습니다.");
+
+    private final String code;
+    private final String message;
+    private final int status;
+
+    ErrorCode(final int status, final String code, final String message) {
+        this.status = status;
+        this.message = message;
+        this.code = code;
+    }
+
+}
---
+++ src/main/java/com/internalemployeeportal/global/payload/ErrorResponse.java
@@ -0,0 +1,41 @@
+package com.internalemployeeportal.global.payload;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Schema(description = "API 오류 응답 포맷")
+public class ErrorResponse {
+
+    @Schema(description = "HTTP 상태 코드", example = "404")
+    private int status;
+
+    @Schema(description = "에러 메시지", example = "존재하지 않는 사용자입니다.")
+    private String message;
+
+    @Schema(description = "비즈니스 에러 코드", example = "U002")
+    private String code;
+
+    private ErrorResponse(ErrorCode errorCode) {
+        this.status = errorCode.getStatus();
+        this.message = errorCode.getMessage();
+        this.code = errorCode.getCode();
+    }
+
+    private ErrorResponse(ErrorCode errorCode, String customMessage) {
+        this.status = errorCode.getStatus();
+        this.message = customMessage;
+        this.code = errorCode.getCode();
+    }
+
+    public static ErrorResponse of(ErrorCode errorCode) {
+        return new ErrorResponse(errorCode);
+    }
+
+    public static ErrorResponse of(ErrorCode errorCode, String customMessage) {
+        return new ErrorResponse(errorCode, customMessage);
+    }
+}
