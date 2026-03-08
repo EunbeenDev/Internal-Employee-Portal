05bc6ab50a368ebfa2103af5beb37b8be49c856e
EunbeenDev <ros4312@naver.com>
2026-03-08 16:50:29 +0900

    feat: spring security에 필요한 설정 추가

---
+++ src/main/java/com/internalemployeeportal/global/config/security/token/UserPrincipal.java
@@ -0,0 +1,96 @@
+package com.internalemployeeportal.global.config.security.token;
+
+import com.internalemployeeportal.domain.user.domain.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class UserPrincipal implements UserDetails {
+
+    private Long id;
+    private String accountId;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+    private Map<String, Object> attributes;
+
+    public UserPrincipal(Long id, String accountId, String password, Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.accountId = accountId;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    public static UserPrincipal create(User user) {
+
+        List<GrantedAuthority> authorities = List.of(
+                new SimpleGrantedAuthority("ROLE_" + user.getRole().getValue())
+        );
+
+        return new UserPrincipal(
+                user.getUserId(),
+                user.getAccountId(),
+                user.getPasswordHash(),
+                authorities
+        );
+    }
+
+    public static UserPrincipal create(User user, Map<String, Object> attributes) {
+        UserPrincipal userPrincipal = UserPrincipal.create(user);
+        userPrincipal.setAttributes(attributes);
+        return userPrincipal;
+    }
+
+    public void setAttributes(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return accountId;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
