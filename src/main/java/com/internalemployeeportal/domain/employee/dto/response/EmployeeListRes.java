c467813c38ad38c3f572e90db60d19e2d2dbdba6
EunbeenDev <ros4312@naver.com>
2026-03-09 14:44:54 +0900

    fix: 고용 상태를 UI에서 확인할 수 있도록 수정

--- src/main/java/com/internalemployeeportal/domain/employee/dto/response/EmployeeListRes.java
+++ src/main/java/com/internalemployeeportal/domain/employee/dto/response/EmployeeListRes.java
@@ -1,5 +1,6 @@
 package com.internalemployeeportal.domain.employee.dto.response;
 
+import com.internalemployeeportal.domain.employee.domain.EmployeeStatus;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -20,4 +21,11 @@ public class EmployeeListRes {
 
     @Schema(description = "소속 부서", example = "인사팀")
     private String department;
+
+    @Schema(description = "고용 상태", example = "EMPLOYED/TERMINATED")
+    private EmployeeStatus employeeStatus;
+
+    @Schema(description = "퇴사 여부", example = "true")
+    private Boolean isTerminated;
+
 }
